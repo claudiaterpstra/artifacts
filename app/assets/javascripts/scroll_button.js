d4f4ec1fd61004a8e8cae1bff267fff800666c31
Claudia Terpstra <claudia_terpstra@hotmail.com>
2017-08-19 16:02:09 +0100

    home-page restyled

--- app/assets/javascripts/scroll_button.js
+++ app/assets/javascripts/scroll_button.js
@@ -3,3 +3,9 @@ $("#button").click(function() {
         scrollTop: $("#button-scroll").offset().top
     }, 2000);
 });
+
+$("#button-home-age").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#instructions").offset().top
+    }, 2000);
+});
