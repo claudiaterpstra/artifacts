dde07f2a2544cb227e95c85e20dad5f57863c999
Claudia Terpstra <claudia_terpstra@hotmail.com>
2017-09-28 17:26:23 +0100

    added strong paramters for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   after_filter :store_location
 
   before_action :better_errors_hack, if: -> { Rails.env.development? }
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   include Pundit
   # Pundit: white-list approach.
@@ -32,6 +33,14 @@ class ApplicationController < ActionController::Base
 
   def default_url_options
     { host: ENV["HOST"] || "localhost:3000" }
+  end
+
+  def configure_permitted_parameters
+    ## To permit attributes while registration i.e. sign up (app/views/devise/registrations/new.html.erb)
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:first_name, :last_name])
+
+    ## To permit attributes while editing a registration (app/views/devise/registrations/edit.html.erb)
+    devise_parameter_sanitizer.permit(:account_update, keys:[:first_name, :last_name])
   end
 
   private
