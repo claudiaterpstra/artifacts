5dca8fb5bc4741c2a87e3c6c7386aebc13d1320b
Claudia Terpstra <claudia_terpstra@hotmail.com>
2017-10-05 17:40:05 +0100

    changed production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     user_name:      ENV['SENDMAIL_USERNAME'],
     password:       ENV['SENDMAIL_PASSWORD'],
-    domain:         ENV['heroku.com'],
+    domain:         'gmail.com',
     address:       'smtp.gmail.com',
     port:          '587',
     authentication: :plain,
